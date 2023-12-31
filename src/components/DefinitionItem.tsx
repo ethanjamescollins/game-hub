f3882110cac16dc15fb5f2a2a75251958e9a8689
EthanCollins <ethan@ethancollins.me>
2023-12-31 14:04:08 +0000

    Build game attributes.

---
+++ src/components/DefinitionItem.tsx
@@ -0,0 +1,20 @@
+import { Box, GridItem, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+	term: string;
+	children: ReactNode | ReactNode[];
+}
+
+const DefinitionItem = ({ term, children }: Props) => {
+	return (
+		<Box marginY={5}>
+			<Heading as={"dt"} fontSize={"md"} color={"gray.600"}>
+				{term}
+			</Heading>
+			<Text as={"dd"}>{children}</Text>
+		</Box>
+	);
+};
+
+export default DefinitionItem;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -11,11 +14,13 @@ const GameDetailPage = () => {
 
 	if (error || !game) throw error;
 
-    return (
-        <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-        </>);
+	return (
+		<>
+			<Heading>{game.name}</Heading>
+			<ExpandableText>{game.description_raw}</ExpandableText>
+			<GameAttributes game={game} />
+		</>
+	);
 };
 
 export default GameDetailPage;
